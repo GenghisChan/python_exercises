648df6bf6f9a350369f0ee3b20d9d486116b3ebe
github username <github email address>
2018-12-11 13:52:53 -0500

    jupyter notebook is awesome

--- helloworld.py
+++ helloworld.py
@@ -8,6 +8,10 @@ print(10 % 3)
 print(10 ** 10)
 print(10 * 10 + 3 + 1)
 
-
 miss_you = {"hello ": 'there', "the": "angel",  "from": "my", "nightmare": "the shadow"}
-print(miss_you)
+
+hello = "hello"
+
+print(hello[-1])
+
+# jupyter note book is super handy for testing out code
